aecfad86ebd7be980a6135d5bc89a1b39af9ea8e
DevFreddy <michael.frederickjr@gmail.com>
2020-01-27 16:22:48 -0500

    refactor: remove unused AutoSizer and PlatformStateContext providers

--- nerdlets/notebook-nerdlet/index.js
+++ nerdlets/notebook-nerdlet/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import NotebookNerdlet from './main';
-import { PlatformStateContext, NerdletStateContext, AutoSizer } from 'nr1';
+import { NerdletStateContext } from 'nr1';
 
 export default class Wrapper extends React.PureComponent {
   render() {
     return (
-      <PlatformStateContext.Consumer>
-        {platformUrlState => (
-          <NerdletStateContext.Consumer>
-            {nerdletUrlState => (
-              <AutoSizer>
-                {({ width, height }) => (
-                  <NotebookNerdlet
-                    launcherUrlState={platformUrlState}
-                    nerdletUrlState={nerdletUrlState}
-                    width={width}
-                    height={height}
-                  />
-                )}
-              </AutoSizer>
-            )}
-          </NerdletStateContext.Consumer>
+      <NerdletStateContext.Consumer>
+        {nerdletUrlState => (
+          <NotebookNerdlet nerdletUrlState={nerdletUrlState} />
         )}
-      </PlatformStateContext.Consumer>
+      </NerdletStateContext.Consumer>
     );
   }
 }
