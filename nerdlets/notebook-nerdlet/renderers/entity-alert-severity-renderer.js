a827a6cae82b085fe819c26309e15fe853372fc6
DevFreddy <michael.frederickjr@gmail.com>
2020-01-27 10:57:19 -0500

    refactor: eslint/prettier autofixes

--- nerdlets/notebook-nerdlet/renderers/entity-alert-severity-renderer.js
+++ nerdlets/notebook-nerdlet/renderers/entity-alert-severity-renderer.js
@@ -1,45 +1,53 @@
 import React from 'react';
 import { Button } from 'nr1';
-import { normalizeWhitespace } from './util'
-import { searchAncestors } from '../results/util.js'
+import { normalizeWhitespace } from './util';
+import { searchAncestors } from '../results/util.js';
 
 export default class EntityAlertSeverityRenderer extends React.Component {
   static test(node) {
-    return node.__meta.typename == "EntityAlertSeverity" && node.value == "NOT_CONFIGURED"
+    return (
+      node.__meta.typename == 'EntityAlertSeverity' &&
+      node.value == 'NOT_CONFIGURED'
+    );
   }
 
   getEntityGuid() {
-    //TODO: augment response with interfaces as well as typenames so that we can
+    // TODO: augment response with interfaces as well as typenames so that we can
     // just test against something like the AlertableEntity interface
     if (this.props.node.guid && this.props.node.guid.value) {
-      return this.props.node.guid.value
+      return this.props.node.guid.value;
     }
 
-    let guid = searchAncestors(this.props.node,
-      (ancestor) => ancestor.__meta.typename == 'ApmApplicationEntity' ||
-                    ancestor.__meta.typename == 'AlertableEntity' ||
-                    ancestor.__meta.typename == 'ApmApplicationEntityOutline' ||
-                    ancestor.__meta.typename == 'AlertableEntityOutline',
-      (ancestor) =>  ancestor.guid && ancestor.guid.value ||
-                     ancestor.__meta.context.arguments.guid && ancestor.__meta.context.arguments.guid.value)
-    return guid
+    const guid = searchAncestors(
+      this.props.node,
+      ancestor =>
+        ancestor.__meta.typename == 'ApmApplicationEntity' ||
+        ancestor.__meta.typename == 'AlertableEntity' ||
+        ancestor.__meta.typename == 'ApmApplicationEntityOutline' ||
+        ancestor.__meta.typename == 'AlertableEntityOutline',
+      ancestor =>
+        (ancestor.guid && ancestor.guid.value) ||
+        (ancestor.__meta.context.arguments.guid &&
+          ancestor.__meta.context.arguments.guid.value)
+    );
+    return guid;
   }
 
   render() {
-    let entityGuid = this.getEntityGuid();
-    let alertSeverity = this.props.node.value
-    return <>
-      <span className="json-tree-text-field">
-        {alertSeverity}
-      </span>
-      &nbsp;
-      { entityGuid ? this.renderJumpStartButton(entityGuid) : null}
-    </>
+    const entityGuid = this.getEntityGuid();
+    const alertSeverity = this.props.node.value;
+    return (
+      <>
+        <span className="json-tree-text-field">{alertSeverity}</span>
+        &nbsp;
+        {entityGuid ? this.renderJumpStartButton(entityGuid) : null}
+      </>
+    );
   }
 
   renderJumpStartButton(entityGuid) {
-    let { addCell } = this.props.util
-    let suggestedQuery = `
+    const { addCell } = this.props.util;
+    const suggestedQuery = `
 mutation {
   jumpStartApmApplicationEntityAlerts(
     guid: "${entityGuid}",
@@ -49,8 +57,9 @@ mutation {
     policyId
   }
 }
-  `
-    return <Button
+  `;
+    return (
+      <Button
         type={Button.TYPE.NORMAL}
         sizeType={Button.SIZE_TYPE.SMALL}
         iconType={Button.ICON_TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__NODE}
@@ -63,10 +72,11 @@ mutation {
             has a basic configuration, but you can customize them by providing a
             critical and warning threshold. You can also exclude of any of the
             conditions by setting it to null.`)
-          })
+          });
         }}
       >
         Add an alert policy to this entity
       </Button>
+    );
   }
 }
