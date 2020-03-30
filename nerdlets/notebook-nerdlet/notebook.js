29752e53ee51cde7ab6b181c13c768b2021770aa
Joel Worrall <jworrall@newrelic.com>
2020-03-30 10:04:13 -0400

    feat: moving to uuid lib + eslint fixes.

--- nerdlets/notebook-nerdlet/notebook.js
+++ nerdlets/notebook-nerdlet/notebook.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { HeadingText, Button, Stack, StackItem, TextField, Modal } from 'nr1'
+import { HeadingText, Button, Stack, StackItem, TextField, Modal } from 'nr1';
 import NotebookCell from './notebook-cell';
-import { uuidv4 } from "./uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Notebook extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    let emptyCells = [this.createCell()]
+    let emptyCells = [this.createCell()];
     this.state = {
-        title: this.props.ephemeral ? undefined : this.props.title,
-        cells: this.props.cells ?
-          this.props.cells.map((cell) => { return this.createCell(cell) }) :
-          emptyCells,
-        ephemeral: this.props.ephemeral,
-        titleError: false,
-        shareHidden: true,
-        sharedContents: ""
-    }
-}
+      title: this.props.ephemeral ? undefined : this.props.title,
+      cells: this.props.cells
+        ? this.props.cells.map((cell) => {
+            return this.createCell(cell);
+          })
+        : emptyCells,
+      ephemeral: this.props.ephemeral,
+      titleError: false,
+      shareHidden: true,
+      sharedContents: '',
+    };
+  }
 
-createCell = (cell) => {
-  let defaultQuery = `
+  createCell = (cell) => {
+    let defaultQuery = `
 {
   actor {
     user {
@@ -30,178 +32,199 @@ createCell = (cell) => {
     }
   }
 }
-    `.trim()
-  let defaults = {
-    query: defaultQuery,
-    uuid: uuidv4(),
-    domRef: React.createRef(),
-    ref: React.createRef()
-  }
-  return Object.assign(defaults, cell || {})
-}
+    `.trim();
+    let defaults = {
+      query: defaultQuery,
+      uuid: uuidv4(),
+      domRef: React.createRef(),
+      ref: React.createRef(),
+    };
+    return Object.assign(defaults, cell || {});
+  };
 
-onSave = () => {
-  if (this.state.title) {
-    this.props.onSave(this.serialize())
-    this.setState({ephemeral: false})
-  } else {
-    this.setState({titleError: true})
-  }
-}
+  onSave = () => {
+    if (this.state.title) {
+      this.props.onSave(this.serialize());
+      this.setState({ ephemeral: false });
+    } else {
+      this.setState({ titleError: true });
+    }
+  };
 
-onDelete = () => {
-  this.props.onDelete(this.props.uuid)
-}
+  onDelete = () => {
+    this.props.onDelete(this.props.uuid);
+  };
 
-onDeleteCell = (cellUUID) => {
-  let cells = this.state.cells.filter((cell) => {
-    return cell.uuid != cellUUID
-  })
-  this.setState({ cells })
-}
+  onDeleteCell = (cellUUID) => {
+    let cells = this.state.cells.filter((cell) => {
+      return cell.uuid != cellUUID;
+    });
+    this.setState({ cells });
+  };
 
-serialize = () => {
+  serialize = () => {
     let serializedCells = this.state.cells.map((cell) => {
-      return cell.ref.current.serialize()
-    })
+      return cell.ref.current.serialize();
+    });
 
     return {
       cells: serializedCells,
       uuid: this.props.uuid,
-      title: this.state.title
-    }
-}
+      title: this.state.title,
+    };
+  };
 
-onEditTitle = (evt) => {
-  let title = evt.target.value
-  this.setState({ title: title, titleError: false})
-}
+  onEditTitle = (evt) => {
+    let title = evt.target.value;
+    this.setState({ title: title, titleError: false });
+  };
 
-popCell() {
-    this.setState({ cells: this.state.cells.slice(0, -1)})
-}
+  popCell() {
+    this.setState({ cells: this.state.cells.slice(0, -1) });
+  }
 
-addCell = (cell) => {
-  let cells = this.state.cells.map((cell) => {
-    return {...cell, collapsed: true}
-  } )
+  addCell = (cell) => {
+    let cells = this.state.cells.map((cell) => {
+      return { ...cell, collapsed: true };
+    });
 
-  cell.query = cell.query && cell.query.trim()
+    cell.query = cell.query && cell.query.trim();
 
-  let newCell = this.createCell(cell)
+    let newCell = this.createCell(cell);
 
-  cells.push(newCell)
+    cells.push(newCell);
 
-  this.setState({ cells: cells}, () => newCell.domRef.current.scrollIntoView())
-}
+    this.setState({ cells: cells }, () =>
+      newCell.domRef.current.scrollIntoView()
+    );
+  };
 
-updateCell = (cellUUID, cellUpdate) => {
+  updateCell = (cellUUID, cellUpdate) => {
     let cells = this.state.cells.map((cell) => {
       if (cell.uuid == cellUUID) {
-        return Object.assign({}, cell, cellUpdate)
+        return Object.assign({}, cell, cellUpdate);
       }
-      return cell
-    })
-    this.setState({ cells })
-}
+      return cell;
+    });
+    this.setState({ cells });
+  };
 
-renderNotebookToolbar() {
-    return <div className="notebook-tool-bar">
+  renderNotebookToolbar() {
+    return (
+      <div className="notebook-tool-bar">
         <TextField
-          style={{ fontSize: "20px", marginBottom: "14px" }}
-          label='Notebook Name'
-          placeholder='My Great Notebook'
-          className={ this.state.titleError ? "notebook-name-error" : ""}
+          style={{ fontSize: '20px', marginBottom: '14px' }}
+          label="Notebook Name"
+          placeholder="My Great Notebook"
+          className={this.state.titleError ? 'notebook-name-error' : ''}
           value={this.state.title || ''}
-          onChange={this.onEditTitle} />
+          onChange={this.onEditTitle}
+        />
         <Stack fullWidth gapType={Stack.GAP_TYPE.BASE}>
-            <StackItem grow={true}>
-                <Button
-                    onClick={() => this.addCell({})}
-                    type={Button.TYPE.PRIMARY}
-                    iconType={Button.ICON_TYPE.DOCUMENTS__DOCUMENTS__FILE__A_ADD}>
-                    Add new Query
-                </Button>
-            </StackItem>
-            <StackItem>
-                <Button
-                    onClick={this.onSave}
-                    type={Button.TYPE.PRIMARY}
-                    iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__DOWNLOAD}>
-                    Save this Notebook
-                </Button>
-                <Button
-                  style={{ marginLeft: "14px" }}
-                  onClick={this.onDelete}
-                  type={Button.TYPE.DESTRUCTIVE}
-                  disabled={this.state.ephemeral}
-                  iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__TRASH}>
-                  Delete this Notebook
-                </Button>
-                <Button
-                    style={{ marginLeft: "14px" }}
-                    disabled={ this.state.ephemeral }
-                    onClick={() => {
-                      this.setState({shareHidden: false, sharedContents: this.serialize()})
-                    }}
-                    type={Button.TYPE.NORMAL}
-                    iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__SHARE_LINK}>
-                    Share this Notebook
-                </Button>
-            </StackItem>
+          <StackItem grow={true}>
+            <Button
+              onClick={() => this.addCell({})}
+              type={Button.TYPE.PRIMARY}
+              iconType={Button.ICON_TYPE.DOCUMENTS__DOCUMENTS__FILE__A_ADD}
+            >
+              Add new Query
+            </Button>
+          </StackItem>
+          <StackItem>
+            <Button
+              onClick={this.onSave}
+              type={Button.TYPE.PRIMARY}
+              iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__DOWNLOAD}
+            >
+              Save this Notebook
+            </Button>
+            <Button
+              style={{ marginLeft: '14px' }}
+              onClick={this.onDelete}
+              type={Button.TYPE.DESTRUCTIVE}
+              disabled={this.state.ephemeral}
+              iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__TRASH}
+            >
+              Delete this Notebook
+            </Button>
+            <Button
+              style={{ marginLeft: '14px' }}
+              disabled={this.state.ephemeral}
+              onClick={() => {
+                this.setState({
+                  shareHidden: false,
+                  sharedContents: this.serialize(),
+                });
+              }}
+              type={Button.TYPE.NORMAL}
+              iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__SHARE_LINK}
+            >
+              Share this Notebook
+            </Button>
+          </StackItem>
         </Stack>
-    </div>
-}
-closeModal = () => {
-  this.setState({shareHidden: true})
-}
+      </div>
+    );
+  }
+  closeModal = () => {
+    this.setState({ shareHidden: true });
+  };
 
-render() {
-    let { cells } = this.state
-    return <div>
+  render() {
+    let { cells } = this.state;
+    return (
+      <div>
         {this.renderNotebookToolbar()}
         {cells.map((cell, i) => {
-          return <NotebookCell
-                    ref={cell.ref}
-                    domRef={cell.domRef}
-                    key={cell.uuid}
-                    cellIndex={i}
-                    uuid={cell.uuid}
-                    schema={this.props.schema}
-                    query={cell.query}
-                    notes={cell.notes}
-                    collapsed={cell.collapsed}
-                    addCell={this.addCell}
-                    onExpand={() => this.updateCell(cell.uuid, {collapsed: false})}
-                    onCollapse={() => this.updateCell(cell.uuid, {collapsed: true})}
-                    onChange={() => { this.serialize() }}
-                    onDelete={() => this.onDeleteCell(cell.uuid)}
-                    accounts={ this.props.accounts }
-                />
+          return (
+            <NotebookCell
+              ref={cell.ref}
+              domRef={cell.domRef}
+              key={cell.uuid}
+              cellIndex={i}
+              uuid={cell.uuid}
+              schema={this.props.schema}
+              query={cell.query}
+              notes={cell.notes}
+              collapsed={cell.collapsed}
+              addCell={this.addCell}
+              onExpand={() => this.updateCell(cell.uuid, { collapsed: false })}
+              onCollapse={() => this.updateCell(cell.uuid, { collapsed: true })}
+              onChange={() => {
+                this.serialize();
+              }}
+              onDelete={() => this.onDeleteCell(cell.uuid)}
+              accounts={this.props.accounts}
+            />
+          );
         })}
 
-        {
-          cells.length > 1 && <div className="notebook-tool-bar">
-              <Button
-                  onClick={() => this.addCell({})}
-                  type={Button.TYPE.PRIMARY}
-                  iconType={Button.ICON_TYPE.DOCUMENTS__DOCUMENTS__FILE__A_ADD}>
-                  Add new Query
-              </Button>
+        {cells.length > 1 && (
+          <div className="notebook-tool-bar">
+            <Button
+              onClick={() => this.addCell({})}
+              type={Button.TYPE.PRIMARY}
+              iconType={Button.ICON_TYPE.DOCUMENTS__DOCUMENTS__FILE__A_ADD}
+            >
+              Add new Query
+            </Button>
           </div>
-        }
+        )}
 
-       {/*TODO seems to cause an error but things... work...?  */}
-        <Modal hidden={this.state.shareHidden} onClose={ this.closeModal } >
+        {/*TODO seems to cause an error but things... work...?  */}
+        <Modal hidden={this.state.shareHidden} onClose={this.closeModal}>
           <HeadingText>Copy the contents of the box below</HeadingText>
           <textarea
             readOnly
             className="notebook-import-export-box"
-            value={
-              btoa(unescape(encodeURIComponent(JSON.stringify(this.state.sharedContents))))
-            }
+            value={btoa(
+              unescape(
+                encodeURIComponent(JSON.stringify(this.state.sharedContents))
+              )
+            )}
           />
         </Modal>
-    </div>
+      </div>
+    );
   }
 }
